f60883dab23c6d9cbe026460dfdd722c5d10c2b2
Steph Meyering <stephane.meyering@gmail.com>
2020-10-28 13:32:55 -0700

    format code and add comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const staticUrl = 'https://static-links-page.signalnerve.workers.dev'
 const avatarUrl = 'https://media-exp1.licdn.com/dms/image/C5603AQHuRdi4MMKx1w/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=55oEc036kx0xkebQ0SmthbSfiEFjVDRciyDqUfDwHKw'
 const bgUrl = 'https://raw.githubusercontent.com/steph-meyering/portfolio/master/images/altbg.jpg'
+
 const links = [
   {
     name: 'Portfolio',
@@ -35,6 +36,8 @@ const socials = [
   },
 ]
 
+// Headers
+
 const initJSON = {
   headers: { 'content-type': 'application/json;charset=UTF-8' },
 }
@@ -43,16 +46,19 @@ const initHTML = {
   headers: { 'content-type': 'text/html;charset=UTF-8' },
 }
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+// Fetch Listener
+
 /**
- * Respond with hello worker text
+ * Fetch and log a given request object
  * @param {Request} request
  */
 
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
 async function handleRequest(request) {
-  let url = new URL(request.url)
+  const url = new URL(request.url)
   if (url.pathname === '/links') {
     const json = JSON.stringify(links, null, 2)
     return new Response(json, initJSON)
@@ -69,6 +75,9 @@ async function handleRequest(request) {
     return rewriter.transform(res)
   }
 }
+
+
+// Element Handlers
 
 class LinksTransformer {
   constructor(links) {
